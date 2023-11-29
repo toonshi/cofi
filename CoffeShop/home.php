e924ebb204f8f2196770346d431db84af1b9fc20
toonshi <151597210+toonshi@users.noreply.github.com>
2023-11-29 18:33:32 +0300

    Add files via upload

---
+++ CoffeShop/home.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+    <head>
+        <meta name="viewport" content="with=device-width,initial-scale=1.0">
+        <title>cofee haven website</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <section class="header">
+            <nav>
+                <a href="index.html"src="images/logo.png"></a>
+                <div class="nav=links">
+                    <ul>
+                        <li><a href="">HOME</a></li>
+                        <li><a href="">Our Menu</a></li>
+                        <li><a href="">Order</a></li>
+                        <li><a href="">Gifting</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+
+                </div>
+            </nav>
+
+        </section>
+    </body>
+
+
+</html>
+
+
+
+
+
+
+
+        
